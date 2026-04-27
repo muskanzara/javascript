4a20fee53425c44be2431071eaa95bb85b4e6bc5
muskan zara <muskankhokher002@gmail.com>
2026-04-27 15:23:51 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+
+let accountId=12345
+const accountEmail="hechira@gmail.com"
+const accountPassword="rabia@"
+const accountCity="lahore"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert(3+3) // we are using node.js not javascript 
+ let name="Hitesh";
+ let age= 28
+ let isloggedin= "false";
+
+
+// number 2 to the power 53 
+// bigint
+// string=""
+// boolean= true/ false 
+// null------ stanalone value hai 
+// undefined -----not assigned value 
+// symbol -- unique
+// object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); // objecct 
